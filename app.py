4b9f5edd8080597362b1ac22ee3d755cc186cdb9
shreyasda <shreyasshrivatsa@gmail.com>
2024-08-22 16:46:17 +0530

    Created custom POST and GET requests

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ stores = [
 
 #Send a GET request
 @app.get("/store") #http://127.0.0.1:5000/store
-def get_store():
+def get_stores():
     return {"stores": stores}
 
 #Send a POST request
@@ -26,3 +26,30 @@ def create_store(): #http://127.0.0.1:5000/store
     new_store = {"name": request_data["name"], "items": []} #Create a Dictionary
     stores.append(new_store)
     return new_store, 201 #201 is the status code
+
+#Send a custom POST request
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json() #Fetch data from API Client
+    for store in stores:
+        if store["name"] == name: #Check whether store already exists
+            new_item = {"name":request_data["name"], "price":request_data["price"]} # Add new item to the store
+            store["items"].append(new_item) 
+            return new_item, 201
+    return {"message":"Store not found"}, 404 #Display this if the store is not found
+
+#Send a GET request to get the Individual Store details
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message":"Store not found"}, 404 #Display this if the store is not found
+
+#Send a GET request to get the details of an Item in the store
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items":store["items"]}
+    return {"message":"Store not found"}, 404 
