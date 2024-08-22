9c684458b378fd6e83bc87d1e510b10e3eeac976
shreyasda <shreyasshrivatsa@gmail.com>
2024-08-22 15:40:26 +0530

    Sent a POST request

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+stores = [
+    {
+        "name" : "My Store",
+        "items" : [
+            {
+                "name" : "Pen",
+                "price" : 10
+            }
+        ]
+    }
+]
+
+#Send a GET request
+@app.get("/store") #http://127.0.0.1:5000/store
+def get_store():
+    return {"stores": stores}
+
+#Send a POST request
+@app.post("/store")
+def create_store(): #http://127.0.0.1:5000/store
+    request_data = request.get_json() #Fetch data from API Client
+    new_store = {"name": request_data["name"], "items": []} #Create a Dictionary
+    stores.append(new_store)
+    return new_store, 201 #201 is the status code
